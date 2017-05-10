9914259f433a869e3a18965f761c2c9722c2ac9c
Alex Roberts <aerobs@umich.edu>
2017-05-10 16:58:49 -0400

    Added intial collapse for activities

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,5 @@
 function test() {
-    $("h1").append("<p>hi</p>");
-   console.log("Yo")
+    $(".strava").html("<iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/15802442/latest-rides/62468f1b6123fb2a39e4c351e6b03019bef6f932'></iframe>");
 }
 
 test()
