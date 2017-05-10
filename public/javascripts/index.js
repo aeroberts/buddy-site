058a9b702c118f7b54706b9a2272850a4a29c8ad
Alex Roberts <aerobs@umich.edu>
2017-05-10 17:54:29 -0400

    More work on bootstrap collapse for activities.  Event not firing for some reason

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,16 @@
-function test() {
-    $(".strava").html("<iframe height='454' width='300' frameborder='0' allowtransparency='true' scrolling='no' src='https://www.strava.com/athletes/15802442/latest-rides/62468f1b6123fb2a39e4c351e6b03019bef6f932'></iframe>");
-}
+$('.activity-collapse').on('hidden.bs.collapse', function () {
+    console.log("Helloooo");
+    $(this).prev().addClass("Bumps");
+    console.log($(this).prev());
+});
 
-test()
+$('.panel').on('hidden.bs.collapse', function (e) {
+    alert('Event fired on #' + e.currentTarget.id);
+});
+
+
+console.log("Start");
+$('.collapse').collapse({toggle: false});
+console.log("end");
+
+
