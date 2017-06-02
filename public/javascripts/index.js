ec2b157b49ab2228e47138eda666c99770d2bee3
Alex Roberts <aerobs@umich.edu>
2017-06-02 17:53:21 -0400

    Added map resize code for other collapse events

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,22 @@
 // Fetch fitbit summary data on page load
-console.log("what");
+var maps = {}
 $.get("/fitbit_summary")
 .done(function(data) {
     $("#running-container").append(data);
+
+    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    mapboxgl.accessToken = 'pk.eyJ1IjoiYWVyb2JzIiwiYSI6ImNqM2c5djZ0NzAwMGkyd252N2xrMHVmaTgifQ.frda-iPjhvsWct12vzkA-Q';
+
+    $.each($(".fitbit-map"), function() {
+        var id = $(this)[0].id;
+        var map = new mapboxgl.Map({
+            container: id,
+            style: 'mapbox://styles/mapbox/streets-v9',
+            center: [-122.446052, 37.78],
+            zoom: 11
+        });
+        maps[id] = map;
+    });
 })
 .fail(function(err) {
     console.error(err);
@@ -15,6 +29,17 @@ $.get("/fitbit_summary")
 $('#running-container').on('show.bs.collapse', ".activity-collapse", function (e) {
     // Remove rounded edges from bottom
     $(e.target).prev().addClass('act-header-flat-bot');
+});
+
+$('#running-container').on('shown.bs.collapse', ".activity-collapse", function (e) {
+    var fitbitmap = $(e.target).find(".fitbit-map")[0];
+    var id = fitbitmap.id;
+    $(fitbitmap).removeClass("map-hidden");
+    maps[id].resize();
+});
+
+$('#running-container').on('hide.bs.collapse', ".activity-collapse", function (e) {
+    $($(e.target).find(".fitbit-map")[0]).addClass("map-hidden");
 });
 
 $('#running-container').on('hidden.bs.collapse', ".activity-collapse", function (e) {
